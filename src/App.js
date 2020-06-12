39b453c2ac6258ffd0d4de2228e14acc708b8bc4
Stephan Schmidt <stephan@hivomedia.de>
2020-06-12 16:02:04 +0200

    made some changes for implementing Single-Items

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import Header from "./components/Header";
 import MessageList from './components/MessageList';
+// import SingleMessageItem from './components/SingleMessageItem';
+import { Route, useRouteMatch } from 'react-router-dom';
 
 import "./App.css";
 
 function App() {
+	const match = useRouteMatch();
 	return (
 		<div className="App">
 			<div className="App-header">
 				<div className="header"><Header /></div>
-				<div className="messagelist"><MessageList /></div>
-				</div>
+				<Route path={`${match.url}/:itemId`}>
+					{/* <SingleMessageItem /> */}
+					not implemented yet
+				</Route>
+				<Route exact path='/'>
+					<div className="messagelist"><MessageList /></div>
+				</Route>
+			</div>
 		</div>
 	);
 }
--- src/components/MessageListItem.jsx
+++ src/components/MessageListItem.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import './MessageListItem.css';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 export default function MessageListItem(props) {
     const { text, date, id_user, name_user, onNameClickHandler } = props;
     const actDate = new Date(date);
-    console.log(date)
+    const match = useRouteMatch();
     return (
 
         <div className="message-list-item">
             <div className="message-list-item__container">
                 <div className="message-list-item__header">
-                    <div className="message-list-item__user-name" onClick={() => onNameClickHandler(id_user)}>{name_user}</div>
+                    <div className="message-list-item__user-name"><Link to={`${match.url}/${id_user}`}>{name_user}</Link></div>
+                    
                     <div className="message-list-item__date">{`${actDate.getDate()}. ${actDate.getMonth()}. ${actDate.getFullYear()}`}</div>
                 </div>
                 <div className="message-list-item__text">{text}</div>
